47b22642ec116fa1d8ac71e2523803c8a5162f66
Swepilot <niklas@swepilot.se>
2025-04-25 12:07:12 -0500

    Update SoundpoolPlugin.kt

--- soundpool/android/src/main/kotlin/pl/ukaszapps/soundpool/SoundpoolPlugin.kt
+++ soundpool/android/src/main/kotlin/pl/ukaszapps/soundpool/SoundpoolPlugin.kt
@@ -14,7 +14,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URI
@@ -26,26 +25,6 @@ internal val loadExecutor: Executor = Executors.newCachedThreadPool()
 internal val uiThreadHandler: Handler = Handler(Looper.getMainLooper())
 
 class SoundpoolPlugin : MethodCallHandler, FlutterPlugin {
-    companion object {
-        @Suppress("unused")
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            var pool = SoundpoolPlugin()
-            pool.onRegister(registrar.context(), registrar.messenger())
-        }
-
-        private const val CHANNEL_NAME = "pl.ukaszapps/soundpool"
-    }
-
-    private fun onRegister(context: Context,  messenger: BinaryMessenger) {
-        application = context.applicationContext
-        val channel = MethodChannel(messenger, CHANNEL_NAME)
-
-        channel.setMethodCallHandler(this)
-
-        // clearing temporary files from previous session
-        with(application.cacheDir) { this?.list { _, name -> name.matches("sound(.*)pool".toRegex()) }?.forEach { File(this, it).delete() } }
-    }
 
     private lateinit var  application : Context
 
